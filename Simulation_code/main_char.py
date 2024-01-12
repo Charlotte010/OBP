6b4549b4613a39d8ac78b89c9bc8979c60ade665
Charlotte Oorthuizen <charlotte.oorthuizen@gmail.com>
2024-01-12 21:26:14 +0100

    whoop

--- Simulation_code/main_char.py
+++ Simulation_code/main_char.py
@@ -21,15 +21,19 @@ import pandas as pd
 from main_char_queue_1 import simulation_qeueue_1
 from main_char_queue_2 import simulation_qeueue_2
 
-amount_beds_available_1 = 3
+#parameters
+amount_beds_available_1 = 50
 amount_beds_available_2 = 120
+max_expected_waiting_time_1 = 3
+max_expected_waiting_time_2 = 5
 
-amount_of_runs = 100000
+#up to us
+amount_of_runs = 10000
 amount_of_simulations = 2
 
 
 
-#info_handled_elderly_queue_1 = multiple_simulations(simulation_qeueue_1,amount_of_runs, amount_beds_available_1, amount_of_simulations)
+info_handled_elderly_queue_1 = multiple_simulations(simulation_qeueue_1,amount_of_runs, amount_beds_available_1, amount_of_simulations)
 info_handled_elderly_queue_2 = multiple_simulations(simulation_qeueue_2,amount_of_runs, amount_beds_available_2, amount_of_simulations)
 
 
@@ -61,6 +65,26 @@ def compute_expected_waiting_time_all_runs(info_handled_elderly_queue_1, waiting
     Excpected_waiting_times = total_expected_waiting_times / total_elderly_handled
     return Excpected_waiting_times
 
+
+def c1_on_max_expected_waiting_time(simulation_qeueue_1, amount_beds_available,info_handled_elderly_queue,waiting, max_expected_waiting_time):
+    
+    queue_1_waiting_time = compute_expected_waiting_time_all_runs(info_handled_elderly_queue, waiting)
+    
+    while queue_1_waiting_time > max_expected_waiting_time:
+        amount_beds_available += 1
+        info_handled_elderly_queue = multiple_simulations(simulation_qeueue_1,amount_of_runs, amount_beds_available, amount_of_simulations)
+        queue_1_waiting_time = compute_expected_waiting_time_all_runs(info_handled_elderly_queue, waiting)
+        
+    return queue_1_waiting_time, amount_beds_available
+
+
+
+c1_queue1_wait, c1_queue1_beds  = c1_on_max_expected_waiting_time(simulation_qeueue_1, amount_beds_available_1, 
+                                                                  info_handled_elderly_queue_1, 'waiting_time', max_expected_waiting_time_1) 
+
+c1_queue2_wait, c1_queue2_beds  = c1_on_max_expected_waiting_time(simulation_qeueue_2, amount_beds_available_2, 
+                                                                  info_handled_elderly_queue_2, 'waiting_time_in_list_3', max_expected_waiting_time_2) 
+                
 #queue_1_waiting_time = compute_expected_waiting_time_all_runs(info_handled_elderly_queue_1, "waiting_time")
 queue_2_waiting_time = compute_expected_waiting_time_all_runs(info_handled_elderly_queue_2, "waiting_time_in_list_3")
 
@@ -73,6 +97,8 @@ queue_2_waiting_time = compute_expected_waiting_time_all_runs(info_handled_elder
 # - Percentage of how often are all the beds occupied?
 # - What is the percentage of people going from W2 to W3? 
 # - Check how much average servise time is and compare with waiting
+# - make parameter of percentage how many days should wait
+
 
 
 
