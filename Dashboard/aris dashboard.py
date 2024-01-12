de13b6d5ce59af6bf5e49b4659dbfdafbeee4430
harizanis97 <144372162+harizanis97@users.noreply.github.com>
2024-01-12 22:48:18 +0100

    Update aris dashboard.py

--- Dashboard/aris dashboard.py
+++ Dashboard/aris dashboard.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.express as px
+
 
 def main():
     cs_sidebar()
@@ -11,12 +15,126 @@ def main():
 
     if selected_scenario == 'Low Complex & Respite Care':
         cs_body_low_respite(bed_sharing_option)
+        plot_low_complex_chart()
     elif selected_scenario == 'High Complex & Geriatric Rehabilitation':
         cs_body_high_complex(bed_sharing_option)
+        plot_high_complex_chart()
+
+        # Add a button to run the simulation
+    if st.button("Run Simulation"):
+        run_simulation(selected_scenario, bed_sharing_option)
+
 
 def cs_sidebar():
-    # Add any sidebar components if needed
-    pass
+    with st.sidebar:
+        st.write('**Low complex**')
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write('Arrival rate GP')
+            st.write('Outflow home rate')
+            st.write('outflow home with adjustments rate')
+            st.write('Outflow long term care rate')
+            st.write('outflow geriatric care rate')
+            st.write('Outflow hospice rate')
+            st.write('outflow death rate')
+        with col2:
+            st.number_input('rate GP for low complex', min_value=0.0, max_value=None, value=1.34, label_visibility='collapsed')
+            st.number_input("home rate for low complex", min_value=0.0, max_value=1.0,
+                        value=0.7, label_visibility='collapsed')
+            st.number_input("home with adjustments rate for low complex", min_value=0.0, max_value=1.0,
+                        value=0.14, label_visibility='collapsed')
+            st.number_input("Outflow long term care rate for low complex", min_value=0.0, max_value=1.0,
+                        value=0.1, label_visibility='collapsed')
+            st.number_input("outflow geriatric care rate for low complex", min_value=0.0, max_value=1.0,
+                        value=0.02, label_visibility='collapsed')
+            st.number_input("Outflow hospice rate for low complex", min_value=0.0, max_value=1.0,
+                        value=0.02, label_visibility='collapsed')
+            st.number_input("Outflow death rate for low complex", min_value=0.0, max_value=1.0,
+                        value=0.02, label_visibility='collapsed')
+
+        st.write('**Respite care**')
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write('Arrival rate')
+            st.write('Outflow home rate')
+            st.write('outflow home with adjustments rate')
+            st.write('Outflow long term care rate')
+            st.write('outflow geriatric care rate')
+            st.write('Outflow hospice rate')
+            st.write('outflow death rate')
+        with col2:
+            st.number_input("rate GP respite", min_value=0.0, max_value=1.0, value=0.57,
+                            label_visibility='collapsed')
+            st.number_input("home rate respite", min_value=0.0, max_value=1.0,
+                            value=0.9, label_visibility='collapsed')
+            st.number_input("home with adjustments rate respite", min_value=0.0, max_value=1.0,
+                            value=0.05, label_visibility='collapsed')
+            st.number_input("Outflow long term care rate respite", min_value=0.0, max_value=1.0,
+                            value=0.03, label_visibility='collapsed')
+            st.number_input("outflow geriatric care rate respite", min_value=0.0, max_value=1.0,
+                            value=0.01, label_visibility='collapsed')
+            st.number_input("Outflow hospice rate respite", min_value=0.0, max_value=1.0,
+                            value=0.005, label_visibility='collapsed')
+            st.number_input("Outflow death rate respite", min_value=0.0, max_value=1.0,
+                            value=0.005, label_visibility='collapsed')
+
+        st.write('**High complex**')
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write('Arrival rate GP')
+            st.write('Arrival rate Emergency Department')
+            st.write('Arrival rate Hospital')
+            st.write('Outflow home rate')
+            st.write('outflow home with adjustments rate')
+            st.write('Outflow long term care rate')
+            st.write('outflow geriatric care rate')
+            st.write('Outflow hospice rate')
+            st.write('outflow death rate')
+        with col2:
+            st.number_input("rate GP high", min_value=0.0, max_value=None, value=1.34,
+                            label_visibility='collapsed')
+            st.number_input("rate ED high", min_value=0.0, max_value=None, value=0.83,
+                            label_visibility='collapsed')
+            st.number_input("rate hospital high", min_value=0.0, max_value=None, value=0.94,
+                            label_visibility='collapsed')
+            st.number_input("home rate respite", min_value=0.0, max_value=1.0,
+                            value=0.578, label_visibility='collapsed')
+            st.number_input("home with adjustments rate respite", min_value=0.0, max_value=1.0,
+                            value=0.107, label_visibility='collapsed')
+            st.number_input("Outflow long term care rate respite", min_value=0.0, max_value=1.0,
+                            value=0.198, label_visibility='collapsed')
+            st.number_input("outflow geriatric care rate respite", min_value=0.0, max_value=1.0,
+                            value=0.034, label_visibility='collapsed')
+            st.number_input("Outflow hospice rate respite", min_value=0.0, max_value=1.0,
+                            value=0.023, label_visibility='collapsed')
+            st.number_input("Outflow death rate respite", min_value=0.0, max_value=1.0,
+                            value=0.06, label_visibility='collapsed')
+
+        st.write('**Geriatric rehabilitation**')
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write('Arrival rate hospital')
+            st.write('Outflow home rate')
+            st.write('outflow home with adjustments rate')
+            st.write('Outflow long term care rate')
+            st.write('outflow geriatric care rate')
+            st.write('Outflow hospice rate')
+            st.write('outflow death rate')
+        with col2:
+            st.number_input("rate hospital Geriatric", min_value=0.0, max_value=None, value=0.54,
+                            label_visibility='collapsed')
+            st.number_input("home rate Geriatric", min_value=0.0, max_value=1.0,
+                            value=0.6, label_visibility='collapsed')
+            st.number_input("home with adjustments rate Geriatric", min_value=0.0, max_value=1.0,
+                            value=0.107, label_visibility='collapsed')
+            st.number_input("Outflow long term care rate Geriatric", min_value=0.0, max_value=1.0,
+                            value=0.21, label_visibility='collapsed')
+            st.number_input("outflow geriatric care rate Geriatric", min_value=0.0, max_value=0.0,
+                            value=0.0, label_visibility='collapsed') # not possible
+            st.number_input("Outflow hospice rate Geriatric", min_value=0.0, max_value=1.0,
+                            value=0.023, label_visibility='collapsed')
+            st.number_input("Outflow death rate Geriatric", min_value=0.0, max_value=1.0,
+                            value=0.06, label_visibility='collapsed')
 
 def cs_scenario_selection():
     # Display a radio button for scenario selection
@@ -39,8 +157,6 @@ def cs_body_low_respite(bed_sharing_option):
 
     st.write('Bed Sharing Option:', bed_sharing_option)
 
-    # voor later
-    st.write('testing')
     slider_beds = st.slider(
         'Select a range of number of beds',
         0, 100, (25, 75))
@@ -62,5 +178,34 @@ def cs_body_high_complex(bed_sharing_option):
 
     # Other input components related to high complex care can be added as needed
 
+
+def plot_low_complex_chart():
+    # This function creates an interactive sunburst chart using Plotly
+    fig = px.sunburst(names=['Number of Beds', 'Number of Nurses'],
+                      parents=['', ''],
+                      values=[8, 8],  # Replace with the actual values
+                      title='Low Complex & Respite Care: Beds and Nurses')
+
+    # Display the chart using st.plotly_chart
+    st.plotly_chart(fig)
+
+def plot_high_complex_chart():
+    # This function creates an interactive sunburst chart using Plotly
+    fig = px.sunburst(names=['Number of Beds', 'Number of Nurses'],
+                      parents=['', ''],
+                      values=[8, 8],  # Replace with the actual values
+                      title='High Complex & Geriatric Rehabiliation: Beds and Nurses')
+
+    # Display the chart using st.plotly_chart
+    st.plotly_chart(fig)
+
+
+def run_simulation(selected_scenario, bed_sharing_option):
+    # Your simulation logic goes here
+    st.write(f"Running simulation for scenario: {selected_scenario}")
+    st.write(f"Bed Sharing Option: {bed_sharing_option}")
+    # Add your simulation code here
+
 if __name__ == '__main__':
     main()
+
